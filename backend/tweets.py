e2bae495418e9187fbb229a1330d549176233cd6
Elyana Quah <elyanaqwc@gmail.com>
2025-01-24 12:51:10 +0800

    update origins in tweets.py

--- backend/tweets.py
+++ backend/tweets.py
@@ -56,8 +56,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "localhost:5173",
-    "https://bullbear-g7x71tgo0-elyanas-projects.vercel.app/",
-    "bullbear-g7x71tgo0-elyanas-projects.vercel.app/"
+    "https://bullbear-fgbpuxvb0-elyanas-projects.vercel.app/",
+    "bullbear-fgbpuxvb0-elyanas-projects.vercel.app/"
 ]
 
 app.add_middleware(
